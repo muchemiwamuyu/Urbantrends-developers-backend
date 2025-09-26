c739f53e29ce14ce6bbf5a207c15bc9eb5e186af
muchemi wamuyu <muchemiedwin68@gmail.com>
2025-09-26 20:52:13 +0300

    updated cors

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import ProjectRoutes from './routes/projects/ProjectRoutes.js';
 import connectDb from './config/database.js';
+import cors from 'cors';
+
+// Enable CORS for all routes
 
 const app = express();
 dotenv.config();
 
 const port = process.env.PORT
+
+app.use(cors());
+
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://developers.urbantrends.dev'],
+    credentials: true
+}))
 
 // Middleware
 app.use(express.json());
